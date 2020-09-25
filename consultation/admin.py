015fefd2c12d18f1d08a9ae009e622c549e5256c
devendraotari <devendra0014@gmail.com>
2020-09-26 01:13:46 +0530

    file label model changed

--- consultation/admin.py
+++ consultation/admin.py
@@ -5,8 +5,13 @@ from .models import (
     PatientProfile,
     GeneralSymptom,
     SpecialityTag,
+    Consultation,
+    FileLabel,
+    XRayField,
 )
-
+admin.site.register(Consultation)
+admin.site.register(FileLabel)
+admin.site.register(XRayField)
 admin.site.register(SpecialityTag)
 admin.site.register(Qualification)
 admin.site.register(DoctorProfile)
